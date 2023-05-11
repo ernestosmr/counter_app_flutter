fecef9520f01b817100af89844d4255346b5aa75
Ernesto <ernesto@rosvep.com>
2023-05-11 16:16:21 -0500

    first commit

---
+++ lib/src/screens/counter_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child:Text('Counter screen'),
+          ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('10', style: TextStyle( fontSize: 160, fontWeight: FontWeight.w100)),
+            Text('Clicks', style: TextStyle( fontSize: 25)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
