6f154f223f297958902484c0abb9e66b7c6f0412
Ernesto <ernesto@rosvep.com>
2023-05-11 21:13:06 -0500

    new changes on screen dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, 
       theme: ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.lightBlue,
+        colorSchemeSeed: Colors.orange,
       ), 
       home:const CounterScreen(),
       );
